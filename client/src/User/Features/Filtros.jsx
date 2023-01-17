5435d0276c6e7981059c8c24b75c4690178c82ed
Dante <dante.nkaddarian@gmail.com>
2023-01-17 09:57:39 -0300

    corregidos filtros para tamaño mobile

--- client/src/User/Features/Filtros.jsx
+++ client/src/User/Features/Filtros.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { sortByPrice, getByFilters } from "../../Redux/Reducer/allProductSlice";
-import { MDBContainer } from "mdb-react-ui-kit";
+import { MDBCol, MDBContainer, MDBRow } from "mdb-react-ui-kit";
 
 export const Filtros = ({ setOrden, setCurrentPage }) => {
   const dispatch = useDispatch();
@@ -26,20 +26,13 @@ export const Filtros = ({ setOrden, setCurrentPage }) => {
     setCurrentPage(1);
     const sorting = e.target.value;
     let sortedProducts = [];
-    console.log(sorting);
-    console.log("se van a ordenar: ", sorting);
-    console.log("productos a ordenar: ", products);
     if (sorting === "asc") {
-      console.log("se van a ordenar ascendente: ", sorting);
       sortedProducts = products.sort((a, b) => a.price - b.price);
     } else if (sorting === "desc") {
-      console.log("se van a ordenar descendente: ", sorting);
       sortedProducts = products.sort((a, b) => b.price - a.price);
     } else {
-      console.log("se van a ordenar por id: ", sorting);
       sortedProducts = products.sort((a, b) => a.id - b.id);
     }
-    console.log(sortedProducts);
     dispatch(sortByPrice(sortedProducts));
     setOrden(`Ordenado ${sorting}`);
   }
@@ -50,11 +43,14 @@ export const Filtros = ({ setOrden, setCurrentPage }) => {
 
   return (
     <MDBContainer fluid className="d-flex justify-content-around mt-5 mb-5">
+      <MDBRow>
+        <MDBCol className="d-flex justify-content-center align-items-center" size="12" md="3">
+         
       <select
         className="form-select"
         onChange={filterHandler}
         name="color"
-        style={{ width: "220px" }}
+        style={{ maxWidth: "250px" }}
       >
         <option value="">Color</option>
         <option value="white">White</option>
@@ -64,23 +60,28 @@ export const Filtros = ({ setOrden, setCurrentPage }) => {
         <option value="yellow">Yellow</option>
         <option value="green">Green</option>
       </select>
+       </MDBCol>
+      <MDBCol className="d-flex justify-content-center align-items-center" size="12" md="3">
+         
       <select
         className="form-select"
         onChange={filterHandler}
         name="gender"
-        style={{ width: "220px" }}
+        style={{ maxWidth: "250px" }}
       >
         <option value="">Gender</option>
         <option value="female">Female</option>
         <option value="male">Male</option>
         <option value="other">Both Genders</option>
       </select>
-
+       </MDBCol>
+<MDBCol className="d-flex justify-content-center align-items-center" size="12" md="3">
+          
       <select
         className="form-select"
         onChange={filterHandler}
         name="category"
-        style={{ width: "220px" }}
+        style={{ maxWidth: "250px" }}
       >
         <option value="">Category</option>
         <option value="shirts">Shirt</option>
@@ -89,16 +90,20 @@ export const Filtros = ({ setOrden, setCurrentPage }) => {
         <option value="jeans">jeans</option>
         <option value="jackets">Jackets</option>
       </select>
-
+      </MDBCol>
+<MDBCol className="d-flex justify-content-center align-items-center" size="12" md="3">
+         
       <select
         className="form-select"
         onChange={sortingHandler}
-        style={{ width: "220px" }}
+        style={{ maxWidth: "250px" }}
       >
         <option value="">Prices</option>
         <option value="asc">Lowest price</option>
         <option value="desc">Highest price</option>
       </select>
+       </MDBCol>
+      </MDBRow>
     </MDBContainer>
   );
 };
